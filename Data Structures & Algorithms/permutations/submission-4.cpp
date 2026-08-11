7a0c3e58abd00536fea76ae1c713653435b94213
Pablo C. <78892059+Chummiestmast33@users.noreply.github.com>
2026-08-11 11:59:28 -0600

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> subsets;
+        dfs(subsets, nums, 0);
+        return subsets;
+    }
+
+   private:
+    void dfs(vector<vector<int>>& subsets, vector<int>& nums, int start) {
+        if (start == nums.size()) {
+            subsets.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++) {
+            swap(nums[start], nums[i]);
+
+            dfs(subsets, nums, start + 1);
+
+            swap(nums[start], nums[i]);
+        }
+    }
+};
